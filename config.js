69242e52038c0313fe83c7c3d3f093a5987d557f
DevotedMap <shadedweaver+1@gmail.com>
2016-02-05 18:28:37 -0800

    map

---
+++ config.js
@@ -0,0 +1,94 @@
+var CONFIG = {
+  "maps": {
+    "lore_isometric_day": {
+      "imageFormat": "jpg",
+      "lastRendered": [
+        1454703004,
+        0,
+        1454703004,
+        0
+      ],
+      "maxZoom": 11,
+      "name": "Devoted - 3d",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        2
+      ],
+      "textureSize": 6,
+      "tileSetGroup": "lore_isometric_t1",
+      "tileSize": 192,
+      "world": "lore",
+      "worldName": "lore"
+    },
+    "lore_topdown_day": {
+      "imageFormat": "jpg",
+      "lastRendered": [
+        1454703004,
+        0,
+        1454703004,
+        0
+      ],
+      "maxZoom": 9,
+      "name": "Devoted - Flat",
+      "renderView": "topdown",
+      "rotations": [
+        0,
+        2
+      ],
+      "textureSize": 3,
+      "tileSetGroup": "lore_topdown_t3",
+      "tileSize": 144,
+      "world": "lore",
+      "worldName": "lore"
+    }
+  },
+  "mapsOrder": [
+    "lore_isometric_day",
+    "lore_topdown_day"
+  ],
+  "tileSetGroups": {
+    "lore_isometric_t1": {
+      "maxZoom": 11,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ]
+    },
+    "lore_topdown_t3": {
+      "maxZoom": 9,
+      "tileOffsets": [
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ]
+    }
+  }
+};
